f5416dd2b6028d6c1ed014d3ec6f157c3689aaf5
Eugene Zaidun <vimo@Eugenes-iMac.local>
2024-05-22 21:52:26 +0200

    chore: wip

--- append-block-children.js
+++ append-block-children.js
@@ -1,5 +1,3 @@
-const callHttp = require("./common/callHttp");
-
 module.exports = function (RED) {
   function AppendBlockChildrenNode(config) {
     RED.nodes.createNode(this, config);
@@ -8,18 +6,24 @@ module.exports = function (RED) {
     node.on("input", async function (msg) {
       node.status({ fill: "gray", shape: "dot", text: "Starting" });
       try {
-        const response = await callHttp(
-          `https://api.notion.com/v1/blocks/${msg.blockId}/children`,
-          "PATCH",
-          this.integration.apiKey,
-          msg.payload
-        );
+        const response = await this.integration.notion.blocks.children.append({
+          block_id: msg.blockId,
+          children: msg.payload.children,
+        });
+
         msg.payload = response;
-        if (response.object === "list") msg.statusCode = 200;
-        else msg.statusCode = response.status;
-        if (msg.statusCode === 200)
+
+        // Check the response status
+        if (response.object === "list") {
           node.status({ fill: "green", shape: "dot", text: "Done" });
-        else node.status({ fill: "red", shape: "dot", text: msg.payload.code });
+        } else {
+          msg.statusCode = response.status;
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: response.code || "API Error",
+          });
+        }
         node.send(msg);
       } catch (error) {
         msg.error = `Unexpected error: ${error.message}`;
